a69798134812b19bf1f7402498f4f5090bcce4c0
kHigasa <kojihigasa@gmail.com>
2020-11-17 11:08:53 +0900

    rebuilding site Tue Nov 17 11:08:53 AM JST 2020

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,6 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t hugo-tranquilpeak-theme
 
-# Go To Public folder
-cd public
-
 # Add changes to git.
 git add .
 
@@ -20,11 +17,3 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push -f origin master:gh-pages
-
-# Come Back up to the Project Root
-cd ..
-
-# Commit source repository changes
-git add .
-git commit -m "$msg"
-git push origin master
